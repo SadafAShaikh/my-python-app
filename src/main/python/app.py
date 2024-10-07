56c6d50cc22de33b1d4dea914e5698d8003aa462
Shaikh Sadaf <127325364+SadafAShaikh@users.noreply.github.com>
2024-10-07 22:26:28 +0530

    app.py

--- src/main/python/app.py
+++ src/main/python/app.py
@@ -1,10 +1,21 @@
+import signal
+import sys
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return "Hello, World!"
+
+# Function to handle graceful shutdown
+def shutdown_signal_handler(sig, frame):
+    print('Shutting down gracefully...')
+    # Perform any cleanup tasks here if needed
+    sys.exit(0)
+
+# Register the signal handler for SIGINT (CTRL+C)
+signal.signal(signal.SIGINT, shutdown_signal_handler)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
